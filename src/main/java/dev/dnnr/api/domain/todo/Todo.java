19d8f37f120b2217d59c221ddb31fea0a97d66e7
Denner Azevedo <denner.gomes@ciahering.com.br>
2024-06-23 09:20:23 -0300

    feat: creating entites users, todo

---
+++ src/main/java/dev/dnnr/api/domain/todo/Todo.java
@@ -0,0 +1,30 @@
+package dev.dnnr.api.domain.todo;
+
+import dev.dnnr.api.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "todos")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String description;
+    private Boolean completed;
+    private Date createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ src/main/java/dev/dnnr/api/domain/user/User.java
@@ -0,0 +1,30 @@
+package dev.dnnr.api.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
