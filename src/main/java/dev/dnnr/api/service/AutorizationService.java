82ae55211657dcbb75d2725c5a0fb6e320e64b4e
Denner Azevedo <denner.gomes@ciahering.com.br>
2024-06-24 21:28:33 -0300

    feat: add auth/user service

---
+++ src/main/java/dev/dnnr/api/service/AutorizationService.java
@@ -0,0 +1,20 @@
+package dev.dnnr.api.service;
+
+import dev.dnnr.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutorizationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repository.findByEmail(email);
+    }
+}
