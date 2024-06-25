ce767200aa0006db0529fdf08dc17338dd05bfa5
Denner Azevedo <denner.gomes@ciahering.com.br>
2024-06-24 21:26:09 -0300

    fix: add user roles

--- src/main/resources/db/migration/V1__create-user-table.sql
+++ src/main/resources/db/migration/V1__create-user-table.sql
@@ -5,5 +5,6 @@ CREATE TABLE users(
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL
+    password VARCHAR(100) NOT NULL,
+    roles VARCHAR(100) NOT NULL
 );
