43654ef43ca99edc31c7097505d6001caa36777a
Denner Azevedo <denner.gomes@ciahering.com.br>
2024-06-24 21:27:34 -0300

    feat: add auth DTOs

---
+++ src/main/java/dev/dnnr/api/domain/user/AuthDTO.java
@@ -0,0 +1,4 @@
+package dev.dnnr.api.domain.user;
+
+public record AuthDTO(String email, String password) {
+}
---
+++ src/main/java/dev/dnnr/api/domain/user/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package dev.dnnr.api.domain.user;
+
+public record LoginResponseDTO(String token) {
+}
