b3016f39af457800a332d0b53a4230fb6519b414
Denner Azevedo <denner.gomes@ciahering.com.br>
2024-06-26 22:19:18 -0300

    fix: to-do endpoint

--- src/main/java/dev/dnnr/api/controller/TodoController.java
+++ src/main/java/dev/dnnr/api/controller/TodoController.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/todos")
+@RequestMapping("/tools/todos")
 public class TodoController {
 
     @Autowired
