7fa60f4a6c04ff56d72ddc915f186e71e684214d
Denner Azevedo <denner.gomes@ciahering.com.br>
2024-06-26 22:18:45 -0300

    fix: indent sql migrations

--- src/main/resources/db/migration/V1__create-user-table.sql
+++ src/main/resources/db/migration/V1__create-user-table.sql
@@ -1,10 +1,11 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-CREATE TABLE users(
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    roles VARCHAR(100) NOT NULL
+CREATE TABLE users
+(
+    id         UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    first_name VARCHAR(50)  NOT NULL,
+    last_name  VARCHAR(50)  NOT NULL,
+    email      VARCHAR(100) NOT NULL UNIQUE,
+    password   VARCHAR(100) NOT NULL,
+    roles      VARCHAR(100) NOT NULL
 );
--- src/main/resources/db/migration/V2__create-todo-table.sql
+++ src/main/resources/db/migration/V2__create-todo-table.sql
@@ -1,8 +1,9 @@
-CREATE TABLE todos(
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+CREATE TABLE todos
+(
+    id          UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     description VARCHAR(200) NOT NULL,
-    completed BOOLEAN NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    user_id UUID,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    completed   BOOLEAN      NOT NULL,
+    created_at  TIMESTAMP    NOT NULL,
+    user_id     UUID         NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
