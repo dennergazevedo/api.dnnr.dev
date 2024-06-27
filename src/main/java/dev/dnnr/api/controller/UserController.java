2e38c548182fb665afadf52837861603fa40b65f
Denner Azevedo <denner.gomes@ciahering.com.br>
2024-06-27 08:17:37 -0300

    fix: remove unused user endpoints

--- src/main/java/dev/dnnr/api/controller/UserController.java
+++ src/main/java/dev/dnnr/api/controller/UserController.java
@@ -1,7 +1,5 @@
 package dev.dnnr.api.controller;
 
-import dev.dnnr.api.domain.user.User;
-import dev.dnnr.api.domain.user.UserRequestDTO;
 import dev.dnnr.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,18 +13,6 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-
-    @PostMapping
-    public ResponseEntity<User> create(@RequestBody UserRequestDTO body){
-        User newUser = this.userService.createUser(body);
-        return ResponseEntity.ok(newUser);
-    }
-
-    @GetMapping
-    public ResponseEntity<User> getUser(@RequestParam UUID id){
-        User currentUser = this.userService.findUserById(id);
-        return ResponseEntity.ok(currentUser);
-    }
 
     @DeleteMapping
     public ResponseEntity<Void> delete(@RequestParam UUID id){
